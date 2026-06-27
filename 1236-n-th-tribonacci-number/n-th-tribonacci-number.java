8c47c33e3acb6c14fe26110b9ecdc12db1f8bd4d
chetan-akolkar <chetanakolkar03@gmail.com>
2026-06-27 23:37:38 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (15.58%) - LeetSync

--- 1236-n-th-tribonacci-number/n-th-tribonacci-number.java
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.java
@@ -2,9 +2,7 @@ class Solution {
     public int tribonacci(int n) {
         if(n==0){
             return 0;
-        }else if(n==1){
-            return 1;
-        }else if(n==2){
+        }else if(n==1 || n==2){
             return 1;
         }else{
             int firstTerm=0;
